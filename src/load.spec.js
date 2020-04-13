26a23a556a53a4853dfff41f4b83122a1ad17922
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-04-14 00:17:40 +0200

    Convert unit tests to ava

--- src/load.spec.js
+++ src/load.spec.js
@@ -1,195 +1,151 @@
 'use strict'
-const { describe, Try } = require('riteway')
+const test = require('ava')
 const PyonError = require('./pyon-error')
-const { load, safeLoad } = require('./load')
+const { load } = require('./load')
 
-describe('load()', async assert => {
-  let should = 'throw a PyonError'
-  assert({
-    given: 'no arguments',
-    should,
-    actual: Try(load),
-    expected: PyonError.emptyValue()
-  })
-  assert({
-    given: 'null',
-    should,
-    actual: Try(load, null),
-    expected: PyonError.emptyValue()
-  })
-  assert({
-    given: 'an integer',
-    should,
-    actual: Try(load, 42),
-    expected: PyonError.noString(42)
-  })
-  assert({
-    given: 'an integer',
-    should,
-    actual: safeLoad('42', true),
-    expected: { name: 'none', body: '42' }
-  })
-  assert({
-    given: 'a number',
-    should,
-    actual: Try(load, 3.14),
-    expected: PyonError.noString(3.14)
-  })
-  assert({
-    given: 'an object',
-    should,
-    actual: Try(load, {}),
-    expected: PyonError.noString({})
-  })
-  assert({
-    given: 'an array',
-    should,
-    actual: Try(load, []),
-    expected: PyonError.noString([])
-  })
-  assert({
-    given: 'improper PyON string',
-    should: 'throw',
-    actual: Try(load, 'xyz'),
-    expected: PyonError.parsingError()
-  })
-  should = 'load the PyON input and return the value'
-  assert({
-    given: 'PyON without surrounding whitespace',
-    should,
-    actual: load('PyON 1 Test\n42\n---'),
-    expected: { name: 'Test', body: 42 }
-  })
-  assert({
-    given: 'PyON without surrounding whitespace',
-    should,
-    actual: safeLoad('PyON 1 Test\n42\n---'),
-    expected: { name: 'Test', body: 42 }
-  })
-  assert({
-    given: 'PyON with surrounding whitespace',
-    should,
-    actual: load('\nPyON 1 Test\n42\n---\n'),
-    expected: { name: 'Test', body: 42 }
-  })
-  assert({
-    given: 'PyON with a null value',
-    should,
-    actual: load('PyON 1 null\nNone\n---').body,
-    expected: null
-  })
-  assert({
-    given: 'PyON with a truthy bool value',
-    should,
-    actual: load('PyON 1 truebool\nTrue\n---').body,
-    expected: true
-  })
-  assert({
-    given: 'PyON with a falsy bool value',
-    should,
-    actual: load('PyON 1 truebool\nFalse\n---').body,
-    expected: false
-  })
-  assert({
-    given: 'PyON with a positive integer value',
-    should,
-    actual: load('PyON 1 null\n42\n---').body,
-    expected: 42
-  })
-  assert({
-    given: 'PyON with a negative integer value',
-    should,
-    actual: load('PyON 1 null\n-42\n---').body,
-    expected: -42
-  })
-  assert({
-    given: 'PyON with a positive float value',
-    should,
-    actual: load('PyON 1 null\n6.023\n---').body,
-    expected: 6.023
-  })
-  assert({
-    given: 'PyON with a negative float value',
-    should,
-    actual: load('PyON 1 null\n-3.14\n---').body,
-    expected: -3.14
-  })
-  assert({
-    given: 'PyON with an empty string value',
-    should,
-    actual: load('PyON 1 null\n""\n---').body,
-    expected: ''
-  })
-  assert({
-    given: 'PyON with a string value',
-    should,
-    actual: load('PyON 1 null\n"xyz"\n---').body,
-    expected: 'xyz'
-  })
-  assert({
-    given: 'PyON with an empty array',
-    should,
-    actual: load('PyON 1 string\n[]\n---').body,
-    expected: []
-  })
-  assert({
-    given: 'PyON with an array without whitespace',
-    should,
-    actual: load('PyON 1 array\n[1,2,3]\n---').body,
-    expected: [1, 2, 3]
-  })
-  assert({
-    given: 'PyON with an array with spaces',
-    should,
-    actual: load('PyON 1 array\n[ 1, 2, 3 ]\n---').body,
-    expected: [1, 2, 3]
-  })
-  assert({
-    given: 'PyON with an array with newlines',
-    should,
-    actual: load('PyON 1 array\n[\n  1,\n  2,\n  3\n]\n---').body,
-    expected: [1, 2, 3]
-  })
-  assert({
-    given: 'PyON with an empty object',
-    should,
-    actual: load('PyON 1 string\n{}\n---').body,
-    expected: {}
-  })
-  assert({
-    given: 'PyON with an object without whitespace',
-    should,
-    actual: load('PyON 1 string\n{"x":1,"y":True}\n---').body,
-    expected: { x: 1, y: true }
-  })
-  assert({
-    given: 'PyON with an object with null property',
-    should,
-    actual: load('PyON 1 string\n{"x":"None"}\n---').body,
-    expected: { x: null }
-  })
-  assert({
-    given: 'PyON with an object with spaces',
-    should,
-    actual: load('PyON 1 string\n{ "x": 1, "y": True }\n---').body,
-    expected: { x: 1, y: true }
-  })
-  assert({
-    given: 'PyON with an object with newlines',
-    should,
-    actual: load('PyON 1 string\n{\n  "x": 1,\n  "y": True\n}\n---').body,
-    expected: { x: 1, y: true }
-  })
-  assert({
-    given: 'pyon array with objects',
-    should,
-    actual: load('PyON 1 objectarray\n[{"a": "xyz"}]\n---').body,
-    expected: [{ a: 'xyz' }]
-  })
-  assert({
-    given: 'pyon array with objects and newlines',
-    should,
-    actual: load('PyON 1 objectarray\n[\n{"a": "xyz" }\n]\n---').body,
-    expected: [{ a: 'xyz' }]
-  })
+test('no input', t => {
+  const error = t.throws(() => load(), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.emptyValue())
+})
+
+test('null input', t => {
+  const error = t.throws(() => load(null), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.emptyValue())
+})
+
+test('integer input', t => {
+  const error = t.throws(() => load(42), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.noString(42))
+})
+
+test('number input', t => {
+  const error = t.throws(() => load(3.14), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.noString(3.14))
+})
+
+test('object input', t => {
+  const error = t.throws(() => load({}), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.noString({}))
+})
+
+test('array input', t => {
+  const error = t.throws(() => load([]), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.noString([]))
+})
+
+test('improper PyON input', t => {
+  const error = t.throws(() => load('xyz'), { instanceOf: PyonError })
+  t.deepEqual(error, PyonError.parsingError())
+})
+
+test('PyON without surrounding whitespace', t => {
+  const input = 'PyON 1 Test\n42\n---'
+  const expected = { name: 'Test', body: 42 }
+  t.deepEqual(load(input), expected)
+})
+
+test('PyON with surrounding whitespace', t => {
+  const input = '\nPyON 1 Test\n42\n---\n'
+  const expected = { name: 'Test', body: 42 }
+  t.deepEqual(load(input), expected)
+})
+
+test('PyON with a null value', t => {
+  const input = 'PyON 1 null\nNone\n---'
+  t.is(load(input).body, null)
+})
+
+test('PyON with a truthy bool value', t => {
+  const input = 'PyON 1 bool\nTrue\n---'
+  t.is(load(input).body, true)
+})
+
+test('PyON with a falsy bool value', t => {
+  const input = 'PyON 1 bool\nFalse\n---'
+  t.is(load(input).body, false)
+})
+
+test('PyON with a positive integer value', t => {
+  const input = 'PyON 1 int\n42\n---'
+  t.is(load(input).body, 42)
+})
+
+test('PyON with a negative integer value', t => {
+  const input = 'PyON 1 int\n-42\n---'
+  t.is(load(input).body, -42)
+})
+
+test('PyON with a positive float value', t => {
+  const input = 'PyON 1 float\n6.023\n---'
+  t.is(load(input).body, 6.023)
+})
+
+test('PyON with a negative float value', t => {
+  const input = 'PyON 1 float\n-3.14\n---'
+  t.is(load(input).body, -3.14)
+})
+
+test('PyON with an empty string value', t => {
+  const input = 'PyON 1 null\n""\n---'
+  t.is(load(input).body, '')
+})
+
+test('PyON with a string value', t => {
+  const input = 'PyON 1 null\n"xyz"\n---'
+  t.is(load(input).body, 'xyz')
+})
+
+test('PyON with an empty array value', t => {
+  const input = 'PyON 1 null\n[]\n---'
+  t.deepEqual(load(input).body, [])
+})
+
+test('PyON with an array without whitespace', t => {
+  const input = 'PyON 1 null\n[1,2,3]\n---'
+  t.deepEqual(load(input).body, [1, 2, 3])
+})
+
+test('PyON with an array with spaces', t => {
+  const input = 'PyON 1 null\n[ 1, 2, 3 ]\n---'
+  t.deepEqual(load(input).body, [1, 2, 3])
+})
+
+test('PyON with an array with newlines', t => {
+  const input = 'PyON 1 null\n[\n  1,\n  2,\n  3\n]\n---'
+  t.deepEqual(load(input).body, [1, 2, 3])
+})
+
+test('PyON with an empty object value', t => {
+  const input = 'PyON 1 null\n{}\n---'
+  t.deepEqual(load(input).body, {})
+})
+
+test('PyON with an object without whitespace', t => {
+  const input = 'PyON 1 null\n{"x":1,"y":True}\n---'
+  t.deepEqual(load(input).body, { x: 1, y: true })
+})
+
+test('PyON with an object with null-value', t => {
+  const input = 'PyON 1 null\n{"x":"None"}\n---'
+  t.deepEqual(load(input).body, { x: null })
+})
+
+test('PyON with an object with spaces', t => {
+  const input = 'PyON 1 null\n{ "x": 1, "y": True }\n---'
+  t.deepEqual(load(input).body, { x: 1, y: true })
+})
+
+test('PyON with an object with newlines', t => {
+  const input = 'PyON 1 string\n{\n  "x": 1,\n  "y": True\n}\n---'
+  t.deepEqual(load(input).body, { x: 1, y: true })
+})
+
+test('pyon array with objects', t => {
+  const input = 'PyON 1 objectarray\n[{"a": "xyz"}]\n---'
+  t.deepEqual(load(input).body, [{ a: 'xyz' }])
+})
+
+test('pyon array with objects and newlines', t => {
+  const input = 'PyON 1 objectarray\n[\n{"a": "xyz" }\n]\n---'
+  t.deepEqual(load(input).body, [{ a: 'xyz' }])
 })
