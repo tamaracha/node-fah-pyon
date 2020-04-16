5c1ae6c550c758048016e03037a54cff1b3ce296
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-04-16 18:11:08 +0200

    Fix lint issue

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,11 +1,5 @@
 'use strict'
 module.exports = {
   '*.{json,yml,yaml}': 'prettier --write',
-  '(lib|config)/**/*.?(c|m)js': files => `standard --fix ${files.join(' ')} | snazzy`,
-  'lib/**/!(*.spec).js': [
-    'tsc --noEmit --allowJs --checkJs --allowSyntheticDefaultImports --moduleResolution node --target es2019 --lib es2019 --module esnext --strict'
-  ],
-  'lib/**/*.spec.js': [
-    'tsc --noEmit --allowJs --checkJs --allowSyntheticDefaultImports --moduleResolution node --target es2019 --lib es2019 --module esnext '
-  ]
+  '(lib|config)/**/*.js': files => `standard --fix ${files.join(' ')} | snazzy`
 }
