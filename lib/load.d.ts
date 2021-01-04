97ddd2881049dd4c2bf7a0c27c1df41ce4d462b8
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2021-01-04 07:44:53 +0100

    Types: Add grammar export to module declaration

--- lib/load.d.ts
+++ lib/load.d.ts
@@ -1,3 +1,5 @@
 export type Message = { type: string; payload: any }
+declare const grammar: nearley.CompiledRules
 export function load(input: string): Message
 export function safeLoad(input: string): Message
+import * as nearley from 'nearley'
