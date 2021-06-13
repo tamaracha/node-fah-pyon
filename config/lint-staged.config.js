ef04bbdf7632f2006b320e2342cf6633b72ef420
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2021-06-13 16:47:17 +0200

    Update dev deps
    - husky, lint-staged, markdownlint, c8, prettier, release-it
    - Fix security vulnerabilities

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   '*.{json,yml,yaml}': 'prettier --write',
   '*.md': 'markdownlint-cli2-fix',
-  '(config|lib)/**/*.?(c|m)js': 'standard --fix'
+  '(config|lib)/**/*.?(c|m)js': ['prettier --write', 'standard --fix']
 }
